65342111d0b6331462a19a24d447fb3a5911bafa
Zihe Zhang <zhangzihe@gmail.com>
2020-01-12 01:45:15 -0500

    Vision api implementation

--- GoogleVisionProject/VisionAPI_Demo.py
+++ GoogleVisionProject/VisionAPI_Demo.py
@@ -4,4 +4,15 @@ from google.cloud.vision import types
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'VisionAIToken'
 
+image_uri = 'gs://cloud-samples-data/vision/using_curl/shanghai.jpeg'
+
 client = vision.ImageAnnotatorClient()
+image = vision.types.Image()
+image.source.image_uri = image_uri
+
+response = client.label_detection(image=image)
+
+print('Labels (and confidence score):')
+print('=' * 79)
+for label in response.label_annotations:
+    print(f'{label.description} ({label.score*100.:.2f}%)')
